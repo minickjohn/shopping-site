4321f69dba8a1194db3127b51607e86dc186f2ed
minickjohn <johnmagaya999@gmail.com>
2021-02-27 02:16:04 +0300

    Shopping system v1

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2020 Storm Studio</b> All rights reserved.
+		</div>
+	</div>
---
+++ includes/footer.php
@@ -0,0 +1,110 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Storm Store</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us"> the first best online store for all Tanzania .</p>
+    
+        <div class="social-icons">
+            
+        <a href="http://facebook.com/johnmagaya" class='active' title="john-magaya"><i class="icon fa fa-facebook"></i></a>
+        <a href="http://twitter.com/johnmagaya" title="storm_official"><i class="icon fa fa-twitter"></i></a>
+        <a href="http://instagram.com/johnmagaya" title="johnmagaya_official"><i class="icon fa fa-instagram"></i></a>
+        <a href="http://linkedin.com/johnmagaya" title="0767186883"><i class="icon fa fa-mobile"></i></a>
+        <a href="http://www.gmail.com/johnmagaya999" title="johnmagaya999@gmail.com"><i class="icon fa fa-envelope"></i></a>
+
+        </div><!-- /.social-icons -->
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT TIMING============================================================= -->
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">opening time</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Jumatatu-Ijumaa:</td><td class="pull-right">08.00 To 11.00</td></tr>
+					<tr><td>Jumamosi:</td><td class="pull-right">09.00 To 11.00</td></tr>
+					<tr><td>Jumapili:</td><td class="pull-right">12.00 To 11.00</td></tr>
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">information</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>Kariakoo, Msimbazi Center</p>
+                    <p>Dar es salaam</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>(255) 767 186 883<br>(255) 784 268 200</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="http://johnmagaya999@gmail.com">johnmagaya999@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+
+    
